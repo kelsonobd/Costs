6d4bcb52751d459abce8ddfbaca1fa00731efc3a
Kelsonobd <kelsonobd@gmail.com>
2024-08-01 23:39:54 -0300

    ajuste

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProjects'
+
+
 function App() {
     return (
-        <div className="App">
-            <p>Costs</p>
-        </div>
+        <Router>
+           <ul>
+            <li>Home</li>
+            <li>Contato</li>
+            </ul> 
+            
+        <Routes>
+            <Route exact path="/">
+            <Home/>
+            </Route>
+            <Route exact path="/company">
+            <Company/>
+            </Route>
+            <Route exact path="/contact">
+            <Contact/>
+            </Route>
+            <Route exact path="/newproject">
+            <NewProject/>
+            </Route>
+        </Routes>
+        </Router>
+        
     )
 }
 
---
+++ src/components/pages/NewProjects.js
@@ -0,0 +1,5 @@
+function NewProjects() {
+    return <h1>New Projects</h1>
+}
+
+export default NewProjects;
