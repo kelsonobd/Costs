ec7d7ce22cd4d702efbf02fac37ece84204b6256
Kelsonobd <kelsonobd@gmail.com>
2024-08-02 12:09:10 -0300

    configurando link paginas

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
-import NewProject from './components/pages/NewProjects'
+import NewProjects from './components/pages/NewProjects'
+import Container from './components/layout/Container'
 
 
 function App() {
     return (
         <Router>
-           <ul>
-            <li>Home</li>
-            <li>Contato</li>
-            </ul> 
-            
-        <Routes>
-            <Route exact path="/">
-            <Home/>
-            </Route>
-            <Route exact path="/company">
-            <Company/>
-            </Route>
-            <Route exact path="/contact">
-            <Contact/>
-            </Route>
-            <Route exact path="/newproject">
-            <NewProject/>
-            </Route>
-        </Routes>
+            <div>
+                <Link to="/">Home</Link>
+                <Link to="/Company">Empresa</Link>
+                <Link to="/Contact">Contato</Link>
+                <Link to="/NewProjects">Novo Projeto</Link>
+            </div>
+
+            <Container customClass="min-height">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/Company" element={<Company />} />
+                    <Route path="/Contact" element={<Contact />} />
+                    <Route path="/NewProjects" element={<NewProjects />} />
+                </Routes>
+
+            </Container>
+            <p>Footer</p>
+
+
+
         </Router>
-        
-    )
+
+    );
 }
 
 export default App;
