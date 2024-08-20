326d37b0bee944d54b701083220b32b4081adfc0
Kelsonobd <kelsonobd@gmail.com>
2024-08-20 15:19:32 -0300

    footer adicionado

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -8,22 +8,22 @@ import logo from '../../img/costs_logo.png'
 function Navbar() {
 
     return (
-        <nav class={Styles.navbar}>
+        <nav className={Styles.navbar}>
             <Container>
                 <Link to="/">
                     <img src={logo} alt="Costs" />
                 </Link>
-                <ul class={Styles.list}>
-                    <li class={Styles.item}>
+                <ul className={Styles.list}>
+                    <li className={Styles.item}>
                         <Link to="/">Home</Link>
                     </li>
-                    <li class={Styles.item}>
+                    <li className={Styles.item}>
                         <Link to="/projects">Projetos</Link>
                     </li>
-                    <li class={Styles.item}>
+                    <li className={Styles.item}>
                         <Link to="/Company">Empresa</Link>
                     </li>
-                    <li class={Styles.item}>
+                    <li className={Styles.item}>
                         <Link to="/Contact">Contato</Link>
                     </li>
                 </ul>
