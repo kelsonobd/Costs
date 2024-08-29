50f5d971c021282b5a1768960df8b1e334ef1664
Kelsonobd <kelsonobd@gmail.com>
2024-08-29 14:41:24 -0300

    formulario finalizado

--- src/components/pages/NewProjects.js
+++ src/components/pages/NewProjects.js
@@ -1,5 +1,14 @@
+import styles from './NewProjects.module.css'
+import ProjectForm from '../project/ProjectForm';
+
 function NewProjects() {
-    return <h1>New Projects</h1>
+    return(
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto ara depois adicionar os serviços</p>
+            <ProjectForm btnText="Criar Projeto" />
+        </div>
+    )
 }
 
 export default NewProjects;
